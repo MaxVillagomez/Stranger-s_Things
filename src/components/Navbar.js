0f92920e66d1a6e62cff3d89c4ec012d287e055c
MaxVillagomez <maxvillagomez5141@gmail.com>
2022-07-21 22:03:23 -0500

    adding styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 
+
 const Navbar = () => {
     return (
         <header className='flex'>
-            <nav>
-                <h3 className='flex-start'>Stranger's Things</h3>
-                <div className='navLinks'>
-                    <Link className={'none'} to="/home">Home</Link>
-                    <Link className={'none'} to="/profile">Profile</Link>
-                    <Link className={'none'} to="/posts">Posts</Link>
+            <nav className='nav-flex'>
+                <h3>Stranger's Things</h3>
+                <div className='nav-links'>
+                    <Link className='none nav-links' to="/home">Home</Link>
+                    <Link className='none' to="/profile">Profile</Link>
+                    <Link className='none' to="/posts">Posts</Link>
                 </div>
             </nav>
-        </header>
+        </header >
     );
 }
 export default Navbar;
